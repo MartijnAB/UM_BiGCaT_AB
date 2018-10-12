a44663640216c3033f018ee6db56bef70e26804e
martijn <martijn.ab@home.nl>
2018-10-12 17:44:20 +0200

    eerst commit

---
+++ test.py
@@ -0,0 +1,6 @@
+print("hallo wereld")
+iKMAAK0UN0FOUT = float(2)
+print(iKMAAK0UN0FOUT)
+def Deraaredfoutefunsiedieheeelllllllllllllllllllllangiiiiiiiiiiiiiiiiiiiiis(bloekp):
+    print(bloekp)
+Deraaredfoutefunsiedieheeelllllllllllllllllllllangiiiiiiiiiiiiiiiiiiiiis(iKMAAK0UN0FOUT+20)
